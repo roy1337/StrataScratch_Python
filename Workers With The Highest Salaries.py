576613f0e77f43972951334ac4a6e52dafdb0a85
Roy <40725093+roy1337@users.noreply.github.com>
2023-07-07 01:53:45 +0300

    Update Workers With The Highest Salaries.py

--- Workers With The Highest Salaries.py
+++ Workers With The Highest Salaries.py
@@ -3,8 +3,27 @@ You have been asked to find the job titles of the highest-paid employees.
 
 Your output should include the highest-paid title or multiple titles with the same salary.
 
-
 #Solution 1
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+df = forbes_global_2010_2014
+df = df[(df['profits'] == df['profits'].max()) & (df['sector'] == 'Financials')]
+df[['company', 'continent']]
+
+
+#Solution 2
+# Import your libraries
+import pandas as pd
+import numpy as np
+# Start writing code
+
+workers_with_max_salary = worker[worker['salary'] == worker['salary'].max()]
+df = pd.merge(workers_with_max_salary,title, left_on = 'worker_id', right_on = 'worker_ref_id')
+df['worker_title']
+
+#Solution 3
 # Import your libraries
 import pandas as pd
 import numpy as np

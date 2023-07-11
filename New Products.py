951ffb67bfa827c6e2f20172210b845a0e23903e
Roy <40725093+roy1337@users.noreply.github.com>
2023-07-11 13:33:51 +0300

    Update New Products.py

--- New Products.py
+++ New Products.py
@@ -2,6 +2,8 @@ You are given a table of product launches by company by year. Write a query to c
 Output the name of the companies and a net difference of net products released for 2020 compared to the previous year.
 
 
+
+#Solution 1                      
 # Import your libraries
 import pandas as pd
 
@@ -15,3 +17,15 @@ new_df = nop19.merge(nop20, how='inner', on='company_name')
 
 new_df['diff'] = new_df['number_of_products_y']-new_df['number_of_products_x']
 new_df[['company_name','diff']]
+
+#Solution 2
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+df = car_launches.groupby(['company_name','year']).size().reset_index(name='product_name')
+df = pd.merge(df, df, how = 'inner', on='company_name')
+df = df.query('year_x == 2020 & year_y == 2019')[['company_name', 'product_name_x', 'product_name_y']]
+
+df['diff'] = df['product_name_x'] - df['product_name_y']
+df[['company_name','diff']]
